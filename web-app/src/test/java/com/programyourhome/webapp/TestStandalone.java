cd75b688db7d50334b4372abbb80d0d8f97c70d9
Erik Mulder <ewjmulder@yahoo.com>
2015-04-18 22:35:29 +0200

    debug info

--- web-app/src/test/java/com/programyourhome/webapp/TestStandalone.java
+++ web-app/src/test/java/com/programyourhome/webapp/TestStandalone.java
@@ -26,6 +26,7 @@ public class TestStandalone extends NanoHTTPD {
 
     public TestStandalone(final String host, final int port) {
         super(host, port);
+        System.out.println("Host: " + host + ", port: " + port);
     }
 
     @Override

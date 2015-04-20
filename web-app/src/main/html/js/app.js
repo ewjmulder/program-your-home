4ff68da04afbfeca197ee05df816c63a8bd84244
Erik Mulder <ewjmulder@yahoo.com>
2015-04-21 00:26:16 +0200

    Introduced requirejs, modularized js files and logic.

---
+++ web-app/src/main/html/js/app.js
@@ -0,0 +1,34 @@
+requirejs.config({
+	baseUrl: 'js/lib',
+	paths: {
+		// Libraries used in the application.
+		jquery: 'jquery-2.1.1.min',
+		mobile: 'jquery.mobile.custom.min',
+		mmenu: 'jquery.mmenu.min.all',
+		rest: 'jquery.rest.min',
+		handlebars: 'handlebars-v3.0.1.min',
+		
+		// Application defined modules.
+		util: '../app/util',
+		pyh: '../app/pyh'
+	},
+	shim: {
+		// Specify that the jQuery plugins depend on jQuery.
+		'mobile': {
+			deps: ['jquery']
+		},
+		'mmenu': {
+			deps: ['jquery']
+		},
+		'rest': {
+			deps: ['jquery']
+		},
+		// Handlebars uses an 'old school' global object, so we need to expose that.
+		'handlebars': {
+			exports: 'Handlebars'
+		}
+	}
+});
+
+// Start by loading the main app file.
+require(['pyh']);
---
+++ web-app/src/main/html/js/app/pyh.js
@@ -0,0 +1,316 @@
+// Start a new require module.
+define(function (require) {
+
+	var $ = require('jquery');
+	var rest = require('rest');
+	var Handlebars = require('handlebars');
+	var mmenu = require('mmenu');
+	var mobile = require('mobile');
+
+	var util = require('./util');
+	
+	/////////////////////////////////////////
+	// Program Your Home global variables  //
+	/////////////////////////////////////////
+	
+	// Map with all page name->object pairs.
+	var pages = {};
+	// Array with all top level pages. Top level is the first level of the menu.
+	var topLevelPages = [];
+	// Map with all template name->object pairs.
+	var templates = {};
+	// The current page that is on screen.
+	var currentPage;
+	// Map with all rest client name->object pairs.
+	var restClients = {};
+	// The modules that are activated in the menu.
+	var activeModules = [];
+	
+	
+	/////////////////////////////////////////
+	// Program Your Home class definitions //
+	/////////////////////////////////////////
+	
+	// Enum-like definition of all Program Your Home modules.
+	var Module = Object.freeze({
+		ACTIVITIES: "activities",
+		LIGHTS: "lights",
+		DEVICES: "devices"
+	});
+	
+	// Definition of a Page class that represents both a menu entry and a content page.
+	var Page = function (name, templateName, menuName, title, isTopLevel, restApiBase, resourceId, subPages) {
+		this.name = name;
+		this.templateName = templateName;
+		this.menuName = menuName;
+		this.title = title;
+		this.isTopLevel = isTopLevel;
+		this.usesRestApi = function () { return restApiBase != null; };
+		if (this.usesRestApi()) {
+			if (resourceId == null) {
+				this.restApiFunction = function () { return restApiBase.read(); };
+			} else {
+				this.restApiFunction = function () { return restApiBase.read(resourceId); };
+			}
+		}
+		this.subPages = subPages != null ? subPages : [];
+		
+		// Register this instance in the pages map and possibly the top level pages array.
+		pages[name] = this;
+		if (this.isTopLevel) {
+			topLevelPages.push(this);
+		}
+	};
+	
+	
+	/////////////////////////////////////////
+	// Program Your Home main functions    //
+	/////////////////////////////////////////
+	
+	// Create a top level page with the given name as default for all naming and title properties.
+	function createTopLevelPage(name) {
+		new Page(name, name, util.capitalizeFirstLetter(name), util.capitalizeFirstLetter(name), true);
+	};
+	
+	// Create a top level page from a module name, using that name for all naming and title properties.
+	function createModuleTopLevelPages(modules) {
+		modules.forEach(function (module) {
+			new Page(module, module, util.capitalizeFirstLetter(module), util.capitalizeFirstLetter(module), true, restClients[module]);
+		});
+	};
+	
+	// Create a function that handles the result of an api call.
+	function createApiDoneFunction() {
+		return function (result) {
+			if (result.success) {
+				refreshCurrentPage();
+			} else {
+				showError("Rest api call returned an error: " + result.error);
+			}
+		};
+	};
+	
+	// Create a function that handles the result of a async function failure.
+	// The source is a small description of what the async function was loading.
+	function createFailFunction(source) {
+		return function (jqXHR, status, error) {
+			if (error == "") {
+				showError(source + " could not be loaded.");					
+			} else {
+				showError("Loading " + source + " failed with error: " + error);
+			}
+		};
+	};
+	
+	// Load the templates with the given names.
+	function loadTemplates(templateNames) {
+		var templateLoading = $.Deferred();
+		var numberOfTemplatesLoaded = 0;
+		templateNames.forEach(function (templateName) {
+		    $.get("/templates/" + templateName + ".hbs.html", function (contents) {
+		    	try {
+			    	templates[templateName] = Handlebars.compile(contents);
+			    	// Force execution of template to pre-check for any errors.
+			    	templates[templateName]({});
+			    	numberOfTemplatesLoaded++;
+			    	if (numberOfTemplatesLoaded == templateNames.length) {
+			    		templateLoading.resolve();
+			    	}
+		    	} catch (error) {
+		    		showError("Error while compiling template: " + error);
+		    		templateLoading.reject();
+		    	}
+		    })
+		    .fail(createFailFunction("template " + templateName))
+		    .fail(templateLoading.reject);
+		});
+		return templateLoading;
+	}
+	
+	// Load the extra information that is needed to build the complete menu.
+	function loadMenu() {
+		var deviceLoading = $.Deferred();
+		restClients[Module.DEVICES].read().done(function (devices) {
+			// TODO: you might want to add class="ui-link" to the <a>, that is done somewhere (in jquery (ui)) already for the other <a>'s.
+			for (var i = 0; i < devices.length; i++) {
+				pages[Module.DEVICES].subPages.push(new Page("device-" + devices[i].name, "device", devices[i].name, "Device - " + devices[i].name, false, restClients[Module.DEVICES], devices[i].id));
+			}
+			deviceLoading.resolve();
+		})
+	    .fail(createFailFunction("devices"))
+	    .fail(deviceLoading.reject);
+		//TODO: add more menu loading stuff
+		return $.when(deviceLoading);
+	};
+	
+	// Activate the menu, meaning invoking mmenu() on the html list that was build as a basis for that.
+	function activateMenu() {
+		//TODO: Use iconbar extension? (http://mmenu.frebsite.nl/documentation/extensions/iconbar.html)
+		//TODO: check possible usage of all extensions and add-ons! (http://mmenu.frebsite.nl/documentation/addons/)
+		//TODO: Current menu item should be highlighted
+		$("#menu").mmenu({
+			offCanvas	: true,
+			extensions	: ["theme-dark"],
+			header		: {
+				add			: true,
+				update		: true,
+				title		: 'Menu'
+			}
+		});				
+	};
+	
+	// Show an error message to the user.
+	function showError(errorMessage) {
+		// TODO: nicer error 'popup' -> use mmenu (http://mmenu.frebsite.nl/demo/index.html?demo=menu-popup) or some other nice jQuery (dozens of options) solution
+		// TODO: also include a 'report to developer' kind of button to get feedback
+		alert("Error occured: " + errorMessage);
+	};
+		
+	// Load the page with the given name.
+	function loadPage(pageName) {
+		currentPage = pages[pageName];
+		refreshCurrentPage();
+		setTitle(currentPage.title);
+	};
+	
+	// Refresh the current page.
+	function refreshCurrentPage() {
+		if (currentPage != null) {
+			if (currentPage.usesRestApi()) {
+				currentPage.restApiFunction().done(function (data) {
+					setContentWithTemplate(currentPage.templateName, createTemplateDataFromCurrentPage(data));
+				})
+				.fail(createFailFunction("page " + currentPage.name));
+			} else {
+				//TODO: how to provide data input? -> for now just no data required (to be: static template page)
+				setContentWithTemplate(currentPage.templateName, {});
+			}
+		}
+	};
+	
+	// Create a template data input object based on the current page.
+	function createTemplateDataFromCurrentPage(data) {
+		var templateData = {};
+		// Use the template name as property name to feed the template with.
+		templateData[currentPage.templateName] = data; 
+		return templateData;
+	};
+	
+	// Set the content area with the given template.
+	function setContentWithTemplate(templateName, templateData) {
+		var contentHtml = templates[templateName](templateData);
+		$("#content").html(contentHtml);
+	};
+	
+	// Set the title with the given text.
+	function setTitle(title) {
+		$("#title").html(title);
+	};
+	
+	
+	/////////////////////////////////////////////
+	// Program Your Home document ready logic  //
+	/////////////////////////////////////////////
+	
+	function start() {
+		// Search after the 'http://' part (length 7), find the first colon or slash from there gives us our base URL.
+		var indexOfColon = window.location.href.indexOf(":", 7);
+		var indexOfSlash = window.location.href.indexOf("/", 7);
+		var sliceIndex = indexOfColon > -1 ? indexOfColon : indexOfSlash;
+		// Take the same server on port 3737 as base URL for the Program Your Home server.
+		var baseURL = window.location.href.slice(0, sliceIndex) + ":3737/";
+	
+		//TODO: The server can provide a list of activated modules and the UI can enable/disable pages based on that info.
+		//TODO: define module / page filter based on API response from server that tells us what modules are available.
+		activeModules = [Module.ACTIVITIES, Module.LIGHTS, Module.DEVICES];
+	
+		if (util.contains(activeModules, Module.ACTIVITIES)) {
+			var restClientMain = new $.RestClient(baseURL + "main/");
+			restClientMain.add("activities");
+			restClientMain.activities.addVerb("start", "GET");
+			restClientMain.activities.addVerb("stop", "GET");
+			restClients[Module.ACTIVITIES] = restClientMain.activities;
+		}
+	
+		if (util.contains(activeModules, Module.LIGHTS)) {
+			var restClientHue = new $.RestClient(baseURL + "hue/");
+			restClientHue.add("lights");
+			restClientHue.lights.addVerb("on", "GET");
+			restClientHue.lights.addVerb("off", "GET");
+			restClients[Module.LIGHTS] = restClientHue.lights;
+		}
+	
+		if (util.contains(activeModules, Module.DEVICES)) {
+			restClientIr = new $.RestClient(baseURL + "ir/");
+			restClientIr.add("devices");
+			restClients[Module.DEVICES] = restClientIr.devices;
+		}
+		
+		createModuleTopLevelPages(activeModules);
+		createTopLevelPage("settings");
+		createTopLevelPage("about");
+		
+		// To enable recursive template inclusion, used for sub pages.
+		Handlebars.registerPartial("listMenu", $("#listMenu").html());
+		
+		var templateNames = Object.keys(pages).map(function (pageName) {
+			return pages[pageName].templateName;
+		});
+		templateNames.push("device");
+	
+		$.when(loadTemplates(templateNames), loadMenu()).then(function() {
+			// Load the main menu with the menu definition that we now know is available after the pre-loading.
+			var mainMenu = Handlebars.compile($("#listMenu").html());
+			$("#menu").html(mainMenu({ pages: topLevelPages }));
+			// Also, connect all click events to the page onClick function.
+			Object.keys(pages).forEach(function (pageName) {
+				$("#menu-link-" + pageName).click(function () {
+					loadPage(pageName);
+					// Do not return false here, since that interferes with mmenu handling the situation for us.
+				});
+			});
+			// Now we're ready to activate the actual menu on the page.
+			activateMenu();
+			// Load the default page.
+			loadPage("activities");
+			// Refresh every so often to keep in sync with server state.
+			// TODO: Alternative to reload every second: have websocket connection to server and reload only upon receiving a changed event (and ideally only if change is on current page)
+	     setInterval(function () {
+	 		refreshCurrentPage();
+	     }, 1000);
+		}, createFailFunction("menu pre-loading"));
+	};
+
+	// When the document becomes ready, we can start the application.
+	$(document).ready(function () {
+		start();
+	});
+	
+	
+	////////////////////////////////////////////////
+	// Program Your Home module exposed functions //
+	////////////////////////////////////////////////
+
+	return {
+		// TODO: refactor to merge this general on/off behaviour using rest var + verb name
+		// Switch a light on or off.
+		toggleLight: function (id, currentlyOn) {
+			if (currentlyOn) {
+				restClients[Module.LIGHTS].off(id).done(createApiDoneFunction()).fail(createFailFunction("rest api"));
+			} else {
+				restClients[Module.LIGHTS].on(id).done(createApiDoneFunction()).fail(createFailFunction("rest api"));
+			}
+		},
+		// Switch an activity on or off.
+		toggleActivity: function (id, currentlyActive) {
+			if (currentlyActive) {
+				restClients[Module.ACTIVITIES].stop(id).done(createApiDoneFunction()).fail(createFailFunction("rest api"));
+			} else {
+				restClients[Module.ACTIVITIES].start(id).done(createApiDoneFunction()).fail(createFailFunction("rest api"));
+			}
+		}
+	};
+
+		
+// End of require module.
+});

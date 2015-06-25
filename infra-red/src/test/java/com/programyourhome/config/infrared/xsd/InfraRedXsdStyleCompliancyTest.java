9f95eac1e0f15d585beeb27f32f8c890d39cc3fb
Erik Mulder <ewjmulder@yahoo.com>
2015-06-25 14:12:58 +0200

    Added XSD style compliancy tests for infra-red and voice-control

---
+++ infra-red/src/test/java/com/programyourhome/config/infrared/xsd/InfraRedXsdStyleCompliancyTest.java
@@ -0,0 +1,12 @@
+package com.programyourhome.config.infrared.xsd;
+
+import com.programyourhome.common.config.xsd.XsdStyleCompliancyTest;
+
+public class InfraRedXsdStyleCompliancyTest extends XsdStyleCompliancyTest {
+
+    @Override
+    protected String getXsdLocation() {
+        return "/com/programyourhome/config/infra-red/xsd/infra-red.xsd";
+    }
+
+}
---
+++ voice-control/src/test/java/com/programyourhome/config/voicecontrol/xsd/VoiceControlXsdStyleCompliancyTest.java
@@ -0,0 +1,12 @@
+package com.programyourhome.config.voicecontrol.xsd;
+
+import com.programyourhome.common.config.xsd.XsdStyleCompliancyTest;
+
+public class VoiceControlXsdStyleCompliancyTest extends XsdStyleCompliancyTest {
+
+    @Override
+    protected String getXsdLocation() {
+        return "/com/programyourhome/config/voice-control/xsd/voice-control.xsd";
+    }
+
+}

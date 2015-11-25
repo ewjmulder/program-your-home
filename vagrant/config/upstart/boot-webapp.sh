727fcf852ce6ca3b831325c989dd9717cc29f67c
Erik Mulder <ewjmulder@yahoo.com>
2015-11-26 00:27:05 +0100

    Whole provisioning working now! :-)

--- vagrant/config/upstart/boot-webapp.sh
+++ vagrant/config/upstart/boot-webapp.sh
@@ -7,4 +7,4 @@
 # TODO: proper path
 cd /home/pyh/program-your-home/web-app
 
-mvn -l web-app.log exec:java -Dexec.mainClass=com.programyourhome.webapp.TestStandalone -Dexec.args="192.168.2.37 80" -Dexec.classpathScope=test
+mvn exec:java -Dexec.mainClass=com.programyourhome.webapp.TestStandalone -Dexec.args="192.168.2.37 8080" -Dexec.classpathScope=test
